5499535b2057945d7d2cc1cd275ba08c4c36d5ff
i1-kmv <i1-kmv@mail.ru>
2020-02-09 15:19:34 +0200

    init commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+            <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA3_Po1WAynqwcS6GtqJ9Ay-XeEfjXkqBp7qcUh-4Y_4U-w64D'/></div>
+        <div> ava+discription </div>
+        <div>
+            My post
+            <div>
+                new post
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+    </div>
+};
+
+
+
+export default Profile;
