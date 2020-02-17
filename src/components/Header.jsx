7b5d96cbd8f56c42351559f66dbb515f35e15e0a
i1-kmv <i1-kmv@mail.ru>
2020-02-17 12:20:27 +0200

    add CSS-modules for components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Header.module.css';
 
 
 
  const Header = () => {
-     return <header className='header'>
+     return <header className= {s.header}>
          <img src='https://s1.logaster.com/static/v3/img/products/logo.png'/>
      </header>
  }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
             <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA3_Po1WAynqwcS6GtqJ9Ay-XeEfjXkqBp7qcUh-4Y_4U-w64D'/></div>
         <div> ava+discription </div>
         <div>
