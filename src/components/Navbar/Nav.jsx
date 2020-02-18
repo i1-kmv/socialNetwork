5659eddaf3a5b94e039c99ff11274cd893af9548
i1-kmv <i1-kmv@mail.ru>
2020-02-18 12:48:28 +0200

    working with NavLink and activeClassName

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-import s from './Nav.module.css'
+import s from './Nav.module.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Nav = () => {
-    return  <nav className={s.nav}>
-        <div>
-            <a href='/profile'>Profile</a>
-        </div>
-        <div>
-            <a href='/dialogs'>Message</a>
-        </div>
-        <div>
-            <a href='/news'>News</a>
-        </div>
-        <div>
-            <a href='/musik'>Music</a>
-        </div>
-        <div>
-            <a href='/settings'>Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to ='/profile' activeClassName={s.active}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs' activeClassName={s.active}>Message</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news' activeClassName={s.active}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/musik' activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 
 export default Nav;
