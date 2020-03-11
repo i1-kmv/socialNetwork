5e001d6d4d3615fe57b79b05b92fb97c1694401d
i1-kmv <i1-kmv@mail.ru>
2020-03-11 11:50:34 +0200

    36.callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from './render';
+import state from './redux/state.js';
+import {addPost,updateNewPostText} from './redux/state.js';
+import {BrowserRouter, Route} from "react-router-dom";
+import {subscribe} from "./redux/state";
 
 
 
+export let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
 
-rerenderEntireTree();
+
+
+rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
