619075c6176758b0a181908389e362973bf75332
i1-kmv <i1-kmv@mail.ru>
2020-03-30 11:09:04 +0200

    40.action create, action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+
+
 
 
 
@@ -11,13 +14,14 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({ type: 'ADD-POST' });
+        props.dispatch( addPostActionCreator());
     }
 
 
 let onPostChange = () => {
     let text = newPostElement.current.value;
-        props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text });
+        let action =  updateNewPostTextActionCreator(text);
+        props.dispatch(action);
 }
 
 
