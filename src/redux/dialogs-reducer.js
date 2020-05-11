3fc283bbf1a6dfa386c7dbed5e02b741789fe62e
i1-kmv <i1-kmv@mail.ru>
2020-05-11 11:21:13 +0200

    48.Сделал  копию состояния в ProfileReducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -19,18 +19,23 @@ let initialState = {
         {id: 6, message: "Hi"}
     ],
     newMessageBody: ""
-}
+};
 
 const dialogsReducer = (state = initialState , action) => {
     switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
-        case SEND_MESSAGE:
-            let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messages.push({id: 6, message:body});
-            return state;
+        case UPDATE_NEW_MESSAGE_BODY: {
+            let stateCopy = {...state};
+            stateCopy.newMessageBody = action.body;
+            return stateCopy;
+        }
+        case SEND_MESSAGE: {
+            let stateCopy = {...state};
+            stateCopy.messages = [...state.messages]
+            let body = stateCopy.newMessageBody;
+            stateCopy.messages.push({id: 7, message: body});
+            stateCopy.newMessageBody = '';
+            return stateCopy;
+        }
         default:
             return state;
     }
