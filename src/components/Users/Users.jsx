abc6e4b6ddebc3c4b94afe8d760604339cdba9e6
i1-kmv <i1-kmv@mail.ru>
2020-05-18 10:56:19 +0200

    52. Установил axios, сделал get запрос на сервер

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,39 +1,15 @@
 import React from "react";
-import styles from './Users.module.css'
+import styles from './Users.module.css';
+import *as axios from 'axios';
+import userPhoto from "../../assets/images/user.png"
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        });
 
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: 'http://img.kupigolos.ru/parody/5ac489ca3b5dd.jpg?p=voice&s=54015779b5c485f8498fbe32554c0bb5',
-                    follower: false,
-                    fullName: 'Dmitry',
-                    status: 'Im a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'http://img.kupigolos.ru/parody/5ac489ca3b5dd.jpg?p=voice&s=54015779b5c485f8498fbe32554c0bb5',
-                    follower: true,
-                    fullName: 'Sasha',
-                    status: 'Im a boss',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'http://img.kupigolos.ru/parody/5ac489ca3b5dd.jpg?p=voice&s=54015779b5c485f8498fbe32554c0bb5',
-                    follower: false,
-                    fullName: 'Andrew',
-                    status: 'Im a boss',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                }
-
-            ]
-        )
     }
 
     return (
@@ -43,7 +19,7 @@ const Users = (props) => {
                 props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                 </div>
                 <div>
                      {u.follower ?  <button onClick={()=>{props.unfollow(u.id)}}> Unfollow </button>
@@ -53,12 +29,12 @@ const Users = (props) => {
             </span>
                     <span>
                 <span>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </span>
                 </div>)
