0f991338d0cc30bddca2fb2ce3b26ef161b8e422
i1-kmv <i1-kmv@mail.ru>
2020-05-22 13:15:11 +0200

    54. Переделал компоненту Users из функциональной в классовую

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -13,6 +13,9 @@ const Nav = () => {
                 <NavLink to='/dialogs' activeClassName={s.active}>Message</NavLink>
             </div>
             <div className={s.item}>
+                <NavLink to='/users' activeClassName={s.active}>Users</NavLink>
+            </div>
+            <div className={s.item}>
                 <NavLink to='/news' activeClassName={s.active}>News</NavLink>
             </div>
             <div className={s.item}>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,30 +3,32 @@ import styles from './Users.module.css';
 import *as axios from 'axios';
 import userPhoto from "../../assets/images/user.png"
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             });
         }
-    }
 
-    return (
-        <div>
-            <button onClick={getUsers}>Get users</button>
+
+
+    render() {
+       return <div>
             {
-                props.users.map(u => <div key={u.id}>
+                this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
                     <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                 </div>
                 <div>
                      {u.follower ? <button onClick={() => {
-                             props.unfollow(u.id)
+                             this.props.unfollow(u.id)
                          }}> Unfollow </button>
                          : <button onClick={() => {
-                             props.follow(u.id)
+                             this.props.follow(u.id)
                          }}>Follow</button>}
 
                 </div>
@@ -44,8 +46,7 @@ const Users = (props) => {
                 </div>)
             }
         </div>
-    )
-
-};
+    }
+}
 
 export default Users;
